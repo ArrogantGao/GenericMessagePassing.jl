233ad74f8af43394a0b32ad6ec2ff21f4bfd2bf8
Documenter.jl <documenter@juliadocs.github.io>
2025-06-03 09:55:26 +0000

    build based on 589f361

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GenericMessagePassing","category":"page"},{"location":"#GenericMessagePassing","page":"Home","title":"GenericMessagePassing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GenericMessagePassing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GenericMessagePassing]","category":"page"}]
+[{"location":"#GenericMessagePassing","page":"Home","title":"GenericMessagePassing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GenericMessagePassing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
